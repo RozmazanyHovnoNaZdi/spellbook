4fbfa88b779d3624426e1dce0893f9cb6b9803e2
elmic <elmic@DESKTOP-QUPQPGL>
2022-06-23 16:16:58 +0200

    Přidejte soubory projektu.

---
+++ spellbook/HelperClass.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpellBook
+{
+    internal class HelperClass
+    {
+        public Random generator = new Random();
+
+        public string[] PoleZTextu(string adresaSouboru)
+        {
+            string obsahTxt = File.ReadAllText(adresaSouboru);
+            string[] PoleZeSouboru = obsahTxt.Split(";");
+
+            return PoleZeSouboru;
+        }
+        public string VygenerujSpellname(string[] poleForma, string[] poleTypy)
+        {
+            int randomName = generator.Next(23);
+            string spellJmeno = $"{poleForma[randomName]} {poleTypy[randomName]}";
+
+            return spellJmeno;
+        }
+        public int VygenerujCislo(int max)
+        {
+            int nahodneCislo = generator.Next(max);
+
+            return nahodneCislo;
+        }
+    }
+}
---
+++ spellbook/Program.cs
@@ -0,0 +1,87 @@
+﻿global using SpellBook;
+
+
+Console.WriteLine("Generátor náhodných spellů");
+List<Spell> seznamSpellu = new List<Spell>();
+HelperClass pomocneMetody = new HelperClass();
+Spell Spell = new Spell();
+string[] poleForma = pomocneMetody.PoleZTextu("txt/spellform.txt");
+string[] poleTypy = pomocneMetody.PoleZTextu("txt/spelltypes.txt");
+
+while (true)
+{
+    Console.WriteLine("1. Vygenerovat nový spell a přidat do Spellbooku");
+    Console.WriteLine("2. Zobrazit Spellbook");
+    Console.WriteLine("3. Smazat poslední záznam ve Spellbooku");
+    Console.WriteLine("4. Smazat celý Spellbook");
+    Console.WriteLine(". Ukončit aplikaci");
+    string input = Console.ReadLine();
+    Console.Clear();
+
+
+
+
+    if (input == "1")
+    {
+        Spell newSpell = new Spell();
+
+        newSpell.Name = pomocneMetody.VygenerujSpellname(poleForma, poleTypy);
+
+        newSpell.ManaCost = pomocneMetody.VygenerujCislo(100);
+
+        seznamSpellu.Add(newSpell);
+
+        Console.Clear();
+    }
+
+
+
+
+
+    else if (input == "2")
+    {
+        foreach (var spell in seznamSpellu)
+        {
+
+            Console.WriteLine($"Spell: {spell.Name}    Mana za spell: {spell.ManaCost}");
+        }
+    }
+
+
+
+
+
+    else if (input == "3")
+    {
+
+
+        int indexPosledniho = seznamSpellu.Count - 1;
+
+        Spell posledni = seznamSpellu[indexPosledniho];
+
+        seznamSpellu.Remove(posledni);
+
+        Console.Clear();
+    }
+
+
+
+
+    else if (input == "4")
+    {
+        seznamSpellu.Clear();
+
+        Console.Clear();
+    }
+
+
+
+
+
+
+
+    else if (input == "")
+    {
+        break;
+    }
+}
